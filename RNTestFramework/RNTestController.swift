c34f49fcfde8baf616d967416cab0078a73ba201
Gethyl Kurian <gethyl.kurian@kindredgroup.com>
2020-12-02 09:30:28 +1100

    feat: initial commit

---
+++ RNTestFramework/RNTestController.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  RNTestFramework
+//
+//  Created by Gethyl Kurian on 2/12/20.
+//
+
+import UIKit
+
+public class RNTestController {
+    public init() {}
+    
+    public func checkIAmLoading() {
+        NSLog("RNTestFramework called.... !!!! ")
+//        let jsCodeLocation = URL(string: "http://localhost:8081/index.bundle?platform=ios")!
+//
+//          let rootView = RCTRootView(
+//              bundleURL: jsCodeLocation,
+//              moduleName: "rn-test",
+//              initialProperties: nil,
+//              launchOptions: nil
+//          )
+//          let vc = UIViewController()
+//          vc.view = rootView
+//          self.present(vc, animated: true, completion: nil)
+    }
+}
---
+++ RNTestFramework/RNTestFramework.h
@@ -0,0 +1,18 @@
+//
+//  RNTestFramework.h
+//  RNTestFramework
+//
+//  Created by Gethyl Kurian on 2/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RNTestFramework.
+FOUNDATION_EXPORT double RNTestFrameworkVersionNumber;
+
+//! Project version string for RNTestFramework.
+FOUNDATION_EXPORT const unsigned char RNTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNTestFramework/PublicHeader.h>
+
+
